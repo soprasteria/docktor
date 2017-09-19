21a1fc988e35480ec3011e838c3ce5455bb470da
Mahieux Pascal <pascal.mahieux@soprasteria.com>
2017-09-19 15:03:32 +0200

    init integration tests

--- server/integration/helpers.go
+++ server/integration/helpers.go
@@ -1,19 +1,101 @@
 package integration
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/docker/docker/api/types/container"
+	containertypes "github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/go-connections/nat"
+	"github.com/moby/moby/client"
+	"github.com/moby/moby/container"
 	log "github.com/sirupsen/logrus"
-	"github.com/soprasteria/dockerapi"
 	"github.com/soprasteria/docktor/cmd"
 )
 
 const (
 	dockerHost = "unix:///var/run/docker.sock"
+	dockerAPIVersion = "1.23"
+	tcpConnection = "tcp"
+	allIP = "0.0.0.0"
 )
 
+// InstantiateDocker instantiates a daemon docker
+func InstantiateDocker() (*client.Client, error) {
+	docker, err := client.NewClient(dockerHost, dockerAPIVersion, nil, make(map[string]string))
+	if err != nil {
+		return nil, err
+	}
+	return docker, nil
+}
+
+// PullImage pulls a docker image
+func PullImage(docker *client.Client, imageName, imageVersion string) error {
+
+	if docker == nil {
+		return fmt.Errorf("docker daemon is nil, the image can't be pulled")
+	}
+	if imageName == "" {
+		return fmt.Errorf("image's name can't be empty")
+	}
+	if imageVersion == "" {
+		return fmt.Errorf("image's version can't be empty")
+	}
+	_, err := docker.ImagePull(
+		context.Background(),
+		fmt.Sprintf("%v:%v", imageName, imageVersion),
+		types.ImagePullOptions {}
+	)
+	return  err
+}
+
+// CreateContainer creates a container from an docker image
+func CreateContainer(docker *client.Client, containerName, portBinding, imageName, imageVersion string) (container.ContainerCreateCreatedBody, error) {
+
+	if docker == nil {
+		return nil, fmt.Errorf("docker daemon is nil, the container can't be created")
+	}
+	portBindings := nat.PortMap{}
+	port := fmt.Sprintf("%v/%s", portBinding, tcpConnection)
+	portBindings[port] = []nat.PortBinding{{HostIP: allIP, HostPort: portBinding}}
+
+	container, err := docker.ContainerCreate(
+		context.Background(),
+		containertypes.Config {
+			Image: fmt.Sprintf("%v:%v", imageName, imageVersion)
+		},
+		containertypes.HostConfig {
+			PortBindings: portBindings
+		},
+		network.NetworkingConfig {},
+		containerName)
+	if err != nil {
+		return nil, err
+	}
+	return container, nil
+}
+
+// StartContainer starts a container
+func StartContainer(docker *client.Client, container *container.ContainerCreateCreatedBody) error {
+
+	if docker == nil {
+		return nil, fmt.Errorf("docker daemon is nil, the container can't be started")
+	}
+	if container == nil {
+		return nil, fmt.Errorf("container is nil, the container can't be started")
+	}
+	err := docker.ContainerStart(
+		context.Background(),
+		container.ID,
+		types.ContainerStartOptions {}
+	)
+	return err
+}
+
+/*
 // InstantiateDocker instantiates a daemon docker
 func InstantiateDocker() (*dockerapi.Client, error) {
 
@@ -24,8 +106,31 @@ func InstantiateDocker() (*dockerapi.Client, error) {
 		return nil, err
 	}
 	return docker, nil
+}*/
+
+// RunContainer runs a container (pull image + create container + start container)
+func RunContainer(docker *client.Client, containerName, imageName, imageVersion, port string) (*container.Container, error) {
+
+	if docker == nil {
+		return nil, fmt.Errorf("docker daemon is nil, the container can't be instantiated")
+	}
+
+	/*docker.ContainerCreate(
+	context.Background(),
+	&container.Config{
+		Image: fmt.Sprintf("%s%s%s", imageName, ":", imageVersion)
+	},
+	&container.HostConfig{
+		PortBindings: ["12345"]
+	},
+	nil,
+	name)*/
+
+	// Start container
+	container.
 }
 
+/*
 // InstantiateContainer instantiates a container with its name, image name, image version and port used
 func InstantiateContainer(docker *dockerapi.Client, name, imageName, imageVersion, port string) (*dockerapi.Container, error) {
 
@@ -45,12 +150,12 @@ func InstantiateContainer(docker *dockerapi.Client, name, imageName, imageVersio
 		return nil, err
 	}
 	return container, nil
-}
-
+}*/
+/*
 // RunContainer runs a container
 func RunContainer(container *dockerapi.Container) error {
 	return container.Run(false)
-}
+}*/
 
 // StartServer starts the docktor server
 func StartServer() {
@@ -83,17 +188,17 @@ func StartServer() {
 	log.Info(fmt.Sprintf("%s%d%s", "Docktor server STARTED in ", count, " ms"))
 }
 
+/*
 // CleanContainer cleans a container by stopping it and removing it
 func CleanContainer(container *dockerapi.Container) error {
 
 	if container == nil {
 		return fmt.Errorf("container is nil, it can't be cleaned")
 	}
-	fmt.Println("test1")
+	log.Info(fmt.Sprintf("Cleaning container ID : %v", container.Container.ID))
 	err := container.StopAndRemove(true)
 	if err != nil {
 		return err
 	}
-	fmt.Println("test2")
 	return nil
-}
+}*/
--- server/integration/tags_test.go
+++ server/integration/tags_test.go
@@ -3,99 +3,114 @@ package integration
 import (
 	"testing"
 
+	"github.com/moby/moby/client"
 	log "github.com/sirupsen/logrus"
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/soprasteria/dockerapi"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-var (
+type TagsSuite struct {
+	suite.Suite
+	docker  *client.Client
 	mongoDB *dockerapi.Container
-	redis   *dockerapi.Container
-)
+}
 
-func TestServer(t *testing.T) {
+// Run all tests
+func TestTagsSuite(t *testing.T) {
+	suite.Run(t, new(TagsSuite))
+}
+
+// Init the docktor server and the docker daemon
+func (suite *TagsSuite) SetupSuite() {
+	/*
+		// Start server
+		log.Info("Start Server")
+		StartServer()*/
 
 	// Instantiate docker daemon
 	log.Info("Instantiate docker daemon")
 	docker, err := InstantiateDocker()
+	suite.docker = docker
 	if err != nil {
-		t.Fatalf("Unable to instantiate the docker daemon %v", err)
+		suite.T().Fatalf("Unable to instantiate the docker daemon %v", err)
+	}
+}
+
+// In the beggining of each test, instantiate and run the mongoDB instance
+func (suite *TagsSuite) SetupTest() {
+
+	if suite.docker == nil {
+		suite.T().Fatalf("docker daemon is nil")
 	}
 
+	// Pull mongoDB image
+	log.Info("Pull mongoDB image")
+	err := PullImage(suite.docker, "mongo", "3.0")
+
+	if err != nil {
+		suite.T().Fatalf("Unable to pull the mongoDB image %v", err)
+	}
+
+	// Create mongoDB container
+	log.Info("Create mongoDB container")
+	res, err := CreateContainer(suite.docker, "mongo-test", "27017", "mongo", "3.0")
+
+	if err != nil {
+		suite.T().Fatalf("Unable to create the mongoDB container %v", err)
+	}
+	log.Info(res)
+
+	// Start mongoDB container
+	err := StartServer(suite.docker, res)
+	if err != nil {
+		suite.T().Fatalf("Unable to start the mongoDB container %v", err)
+	}
 	// Instantiate mongoDB container
-	log.Info("Instantiate mongoDB container")
-	mongoDB, err := InstantiateContainer(docker, "mongo-test", "mongo", "latest", "27017")
+	//log.Info("Instantiate mongoDB container")
+	/*mongoDB, err := InstantiateContainer(suite.docker, "mongo-test", "mongo", "3.0", "27017")
+	/*suite.mongoDB = mongoDB
 	if err != nil {
-		t.Fatalf("Unable to instantiate the mongoDB container %v", err)
+		suite.T().Fatalf("Unable to instantiate the mongoDB container %v", err)
+	}*/
+	/*
+		// Run docktor mongoDB
+		log.Info("Run docktor mongoDB")
+		err = RunContainer(suite.mongoDB)
+		if err != nil {
+			suite.T().Fatalf("Unable to run the mongoDB container %v", err)
+		}*/
+}
+
+// In the end of each test, clean the mongoDB instance
+func (suite *TagsSuite) TearDownTest() {
+
+	/*if suite.mongoDB == nil {
+		suite.T().Fatalf("mongoDB container is nil")
 	}
 
-	// Run docktor mongoDB
-	log.Info("Run docktor mongoDB")
-	err = RunContainer(mongoDB)
+	// Clean container mongoDB
+	log.Info("Clean container mongoDB")
+	err := CleanContainer(suite.mongoDB)
 	if err != nil {
-		t.Fatalf("Unable to run the mongoDB container %v", err)
-	}
+		suite.T().Fatalf("Unable to clean the mongoDB container %v", err)
+	}*/
+}
 
-	// Instantiate redis container
-	log.Info("Instantiate redis container")
-	redis, err := InstantiateContainer(docker, "redis-test", "redis", "latest", "6379")
-	if err != nil {
-		t.Fatalf("Unable to instantiate the redis container %v", err)
-	}
+func (suite *TagsSuite) Test1() {
+	log.Info("Test1")
+	assert.Equal(suite.T(), 1, 1, "1 = 1")
+	log.Info("Fin Test1")
+}
 
-	// Run docktor redis
-	log.Info("Run docktor redis")
-	err = RunContainer(redis)
-	if err != nil {
-		t.Fatalf("Unable to run the redis container %v", err)
-	}
+func (suite *TagsSuite) Test2() {
+	log.Info("Test2")
+	assert.Equal(suite.T(), 2, 2, "2 = 2")
+	log.Info("Fin Test2")
+}
 
-	// Start server
-	log.Info("Start Server")
-	StartServer()
-
-	log.Info("---------Starting integration tests---------")
-	Convey("Starting integration tests", t, func() {
-
-		Convey("Test1", func() {
-			log.Info("test1")
-			So(1, ShouldEqual, 1)
-		})
-		Convey("Test2", func() {
-			log.Info("test2")
-			So(1, ShouldEqual, 1)
-		})
-		Reset(func() {
-
-			// Clean containers mongoDB and redis
-			log.Info("Clean containers mongoDB and redis")
-			CleanContainer(mongoDB)
-			log.Info("oulala")
-			CleanContainer(redis)
-			log.Info("	haha")
-
-			// Instantiate mongoDB container
-			log.Info("Instantiate mongoDB container")
-			mongoDB, _ = InstantiateContainer(docker, "mongo-test", "mongo", "latest", "27017")
-
-			// Run docktor mongoDB
-			log.Info("Run docktor mongoDB")
-			RunContainer(mongoDB)
-
-			// Instantiate Redis container
-			log.Info("Instantiate Redis container")
-			redis, _ := InstantiateContainer(docker, "redis-test", "redis", "latest", "6379")
-
-			// Run docktor redis
-			log.Info("Run docktor redis")
-			RunContainer(redis)
-		})
-	})
-
-	// Clean containers mongoDB and redis
-	log.Info("Clean containers final mongoDB and redis")
-	CleanContainer(mongoDB)
-	log.Info("clean mongoDB OK")
-	CleanContainer(redis)
-	log.Info("clean redis OK")
+func (suite *TagsSuite) Test3() {
+	log.Info("Test3")
+	assert.Equal(suite.T(), 3, 3, "3 = 3")
+	log.Info("Fin Test3")
 }
